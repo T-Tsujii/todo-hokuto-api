83563fa77cb5b4b758aa37bc6c8526e2a444f1b8
T.Tsujii <ttakehisabt@gmail.com>
2021-07-23 19:44:04 +0900

    取得できるタスクをログイン本人のものに限定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
   include ActionController::HttpAuthentication::Token::ControllerMethods
+  before_action :authenticate_user!
+  # before_action :authenticate, if: -> { Rails.env.production? }
 
-  before_action :authenticate, if: -> { Rails.env.production? }
+  # protected
 
-  protected
-
-  def authenticate
-    authenticate_or_request_with_http_token do |token, _options|
-      token == Rails.application.credentials.token
-    end
-  end
+  # def authenticate
+  #   authenticate_or_request_with_http_token do |token, _options|
+  #     token == Rails.application.credentials.token
+  #   end
+  # end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,17 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :update, :destroy]
 
-  # GET /tasks
   def index
-    render json: Task.order(updated_at: :asc)
+    render json: current_user.tasks.order(updated_at: :asc)
   end
 
-  # GET /tasks/1
   def show
     render json: @task
   end
 
-  # POST /tasks
   def create
     task_params = params.permit(:body)
-    task = Task.new(task_params)
+    task = current_user.tasks.new(task_params)
 
     if task.save
       render_task = ActiveModelSerializers::SerializableResource.new(task).serializable_hash
@@ -25,7 +22,6 @@ class TasksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /tasks/1
   def update
     is_completed = params[:isCompleted]
 
@@ -41,15 +37,13 @@ class TasksController < ApplicationController
     end
   end
 
-  # DELETE /tasks/1
   def destroy
     @task.destroy!
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_task
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
 end
