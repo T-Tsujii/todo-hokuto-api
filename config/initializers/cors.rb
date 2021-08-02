59bd80330b84b1533924020645b972d0f1e7365e
T.Tsujii <ttakehisabt@gmail.com>
2021-08-03 08:21:29 +0900

    CORS設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,22 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  # 開発環境
+  allow do
+    origins "http://localhost:3000"
+
+    resource "*",
+             headers: :any,
+             expose: ["access-token", "client", "uid"],
+             methods: [:get, :post, :patch, :delete]
+  end
+
+  # 本番環境
   allow do
     origins "https://t-tsujii.github.io"
 
     resource "*",
              headers: :any,
+             expose: ["access-token", "client", "uid"],
              methods: [:get, :post, :patch, :delete]
+    #  credentials: true
   end
 end
